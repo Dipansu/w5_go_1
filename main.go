6f9c8c14506e1cb9ed4b02be9ddab4f94e32b41c
Dipansu <dipansu95@gmail.com>
2024-09-26 11:24:57 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+// import "fmt"
+
+func main() {
+	// fmt.Println("Hello World!")
+	Greeting()
+	advanceGreeting("Dipansu")
+
+}
+
+// func Greeting() {
+// 	fmt.Println("HEY THERE!")
+// }
+
+// func advanceGreeting(name string) {
+// 	fmt.Printf(" Hello %s\n\n", name)
+// }
