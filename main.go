844ecbf05d135af415268ed1f4a207316d86f7aa
Dipansu <dipansu95@gmail.com>
2024-09-26 13:23:04 -0400

    updated main.go

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ func main() {
 	advanceGreeting("Dipansu")
 	fmt.Println(add(5, 9))
 	q, r := divide(45, 9)
-	fmt.Printf("Quotient : %d, Remainder : %d", q, r)
+	fmt.Printf("Quotient : %d, Remainder : %d\n", q, r)
+	q1, r1 := divide(45, add(2, 4))
+	fmt.Printf("Quotient : %d, Remainder : %d", q1, r1)
 
 }
 
